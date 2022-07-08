9ddeff82d37c022284bf569c26f260176b3218d4
ellagoris <eduardofidera@gmail.com>
2022-07-08 01:28:47 -0300

    feat: update program id

--- app/lib/gen/programId.ts
+++ app/lib/gen/programId.ts
@@ -2,10 +2,10 @@ import { PublicKey } from "@solana/web3.js"
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
 export const PROGRAM_ID_IDL = new PublicKey(
-  "6es4jWC7maTU9ws2c6QRCmXyVYKM5YRj2dFwQCHGyNii"
+  "34iBQjgMbjfuKzNVupMMbwoj8r99eZAoLcxoobH3Uk4b"
 )
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
 export const PROGRAM_ID: PublicKey = new PublicKey(
-  "6es4jWC7maTU9ws2c6QRCmXyVYKM5YRj2dFwQCHGyNii"
+  "34iBQjgMbjfuKzNVupMMbwoj8r99eZAoLcxoobH3Uk4b"
 )
